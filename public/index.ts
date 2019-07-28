65d86e5d24db90e204e777ee941def96284f47b9
Boris Tane <boris.tane@gmail.com>
2019-07-28 01:08:07 +0100

    Fixes error on reloading the page

--- public/index.ts
+++ public/index.ts
@@ -190,7 +190,17 @@ async function getToken() {
   const params = new URLSearchParams(window.location.search);
   const code = params.get("code");
   const state = params.get("state");
-  const { access_token: token } = (await axios.get(`/get-token/?code=${code}&state=${state}`)).data;
+  const refreshToken = localStorage.getItem("refreshToken");
+  if (!refreshToken) {
+    const { access_token: token, refresh_token: refreshToken } = (await axios.get(
+      `/get-token/?code=${code}&state=${state}`
+    )).data;
+    localStorage.setItem("refreshToken", refreshToken);
+    return token;
+  }
+  const { access_token: token } = (await axios.get(
+    `/refresh-token/?refresh_token=${refreshToken}`
+  )).data;
   return token;
 }
 
